05a761fb835e48c9a69614dcc1cfa6f7a0ca0dba
Shelley616 <huishan510@gmail.com>
2013-10-20 14:25:12 +0800

    fix #15

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.save = function(req, res){
+  res.send("Your Scores:" + req.url);
+};
